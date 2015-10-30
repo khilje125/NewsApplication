54684336b5d2d044795b55f6379fbc51fa243390
zunair <zunair@roundedlabs.com>
2015-10-30 17:22:43 +0500

    First Commint App Setup initialize

---
+++ NewsAppAdmin/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewsAppAdmin.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Login/
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: /SignUp/
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        // GET: /Category/
+
+        public ActionResult Category()
+        {
+            return View();
+        }
+
+        // GET: /SearchCategory/
+
+        public ActionResult SearchCategory()
+        {
+            return View();
+        }
+
+        // GET: /AddRssLink/
+
+        public ActionResult AddRssLink()
+        {
+            return View();
+        }
+    }
+}
